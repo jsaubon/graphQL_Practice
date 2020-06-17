53fca0ef1993751232689cde3e4aecda73e1cc0e
Joshua Saubon <joshuasaubon@gmail.com>
2020-06-18 07:05:55 +0800

    algolia

--- app/Lead.php
+++ app/Lead.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Lead extends Model
 {
+    use Searchable;
     protected $guarded = [];
     
     public function user() {
